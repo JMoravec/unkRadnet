9a2144d728b933babd140f976d11c9103944417c
Joshua Moravec <joshua.moravec@gmail.com>
2013-01-25 11:51:41 -0600

    started a python version of calculate

---
+++ rawData/calculate.py
@@ -0,0 +1,14 @@
+def timeToHours{
+}
+
+# set calibration numbers!!
+ALPHACALIBRATION = 1.63
+BETACALIBRATION = 1.15
+
+#file input
+
+#do line by line
+#assign variables
+#write to file
+#write to screen
+#repeat
