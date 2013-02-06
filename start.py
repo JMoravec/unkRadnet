ee7523d4189dc2f1262691db7acbaa374b712bf4
Joshua Moravec <joshua.moravec@gmail.com>
2013-02-05 21:51:52 -0600

    added start.py so user will only have to run one program and not have to worry about moving files

---
+++ start.py
@@ -0,0 +1,28 @@
+import sys, os
+sys.path.append('./rawData')
+import calculate
+from calculate import *
+
+def checkInput():
+	bla = raw_input('Do these numbers match up with data book? [y,n]: ')
+	if bla == 'y' or bla == 'Yes' or bla == 'yes':
+		os.system('mv ' + sys.argv[1] + 'Activity ' + './fitToCurve')
+		os.system('mv ' + sys.argv[1] + ' ./rawData')
+		return True
+	elif bla == 'n' or bla == 'No' or bla == 'no':
+		print('Check your typed and printed data! Removing generated file, run this program again when fixed')
+		os.system('rm ' + sys.argv[1] + 'Activity')
+		return False
+	else:
+		print("You didn't type 'y' or 'n', try again")
+		if checkInput() == True:
+			return True
+		else:
+			return False
+	
+calculate(sys.argv[1])
+
+if checkInput() == True:
+	#run the fit curve program
+	print 'hi'
+
