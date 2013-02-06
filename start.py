1486c8403f1142ef56886eff3f2dd13de28b0dfd
Joshua Moravec <joshua.moravec@gmail.com>
2013-02-05 22:12:15 -0600

    finished start.py so only one program is run for the end user

--- start.py
+++ start.py
@@ -3,10 +3,13 @@ sys.path.append('./rawData')
 import calculate
 from calculate import *
 
+sys.path.append('./fitToCurve')
+import splitData
+from splitData import *
+
 def checkInput():
 	bla = raw_input('Do these numbers match up with data book? [y,n]: ')
 	if bla == 'y' or bla == 'Yes' or bla == 'yes':
-		os.system('mv ' + sys.argv[1] + 'Activity ' + './fitToCurve')
 		os.system('mv ' + sys.argv[1] + ' ./rawData')
 		return True
 	elif bla == 'n' or bla == 'No' or bla == 'no':
@@ -24,5 +27,5 @@ calculate(sys.argv[1])
 
 if checkInput() == True:
 	#run the fit curve program
-	print 'hi'
-
+	fitToCurve(sys.argv[1] + 'Activity')
+	os.system('mv ' + sys.argv[1] + 'Activity ' + './fitToCurve')
